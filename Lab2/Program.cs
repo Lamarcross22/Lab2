f292e9e359466faa5f9054550883e7128ea5979f
Lamarcross22 <41022434+Lamarcross22@users.noreply.github.com>
2018-07-17 14:01:22 -0400

    intial commit

---
+++ Lab2/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This is Lab 2");
+            decimal length;
+            decimal width;
+        
+        Console.WriteLine("please enter your Length");
+           length= decimal.Parse(Console.ReadLine());
+        Console.WriteLine("please enter a Width");
+            width = decimal.Parse(Console.ReadLine());
+
+
+            decimal area = length * width;
+            decimal primeter = (2 * length) + (2 * width);
+           
+            Console.WriteLine($"Your Area is {area}");
+            Console.WriteLine($"Your primeter is {primeter}");
+
+            Console.WriteLine("Would you like to try again? Type Y or N.");
+            string userResponse = Console.ReadLine();
+
+            /*
+            if (userResponse == "Y" || userResponse == "y")
+            {
+                repeat = true;
+            }
+            else if (userResponse == "N" || userResponse == "n")
+            {
+                repeat = false;
+            }
+            */
+        }
+    }
+}
